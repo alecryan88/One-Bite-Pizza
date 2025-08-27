08084ab02d15a62b8e8f2531380330a0a5e4be33
Alec Ryan <alecryan729@gmail.com>
2025-08-26 20:10:27 -0400

    add files

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NAME=one_bite_reviews
+
+docker build -t $NAME .
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
+
 # Pin the runner here so devs/CI just call this script
 uvx --from pre-commit==4.3.0 pre-commit "$@"

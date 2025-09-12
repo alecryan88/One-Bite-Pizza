1689bddff99b5f1caf3e7cff8e289249775f2a58
Alec Ryan <alecryan729@gmail.com>
2025-09-12 08:18:25 -0400

    add CI handling

--- scripts/docker/push.sh
+++ scripts/docker/push.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Constants
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_ACCOUNT_ID=820242944968 #$(aws sts get-caller-identity --query Account --output text)
 REPOSITORY_NAME=one_bite_pizza_reviews
 AWS_REGION=us-east-1
 ECR_REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 FULL_REPOSITORY_NAME=$ECR_REGISTRY/$REPOSITORY_NAME
-
-source ./scripts/docker/build.sh
 
 echo "GIT_TAG: $GIT_TAG"
 echo "MAIN_TAG: $MAIN_TAG"

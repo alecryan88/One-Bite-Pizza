37658284ded9db9d10e41fd978a98e00b00005b0
Alec Ryan <alecryan729@gmail.com>
2025-09-13 08:37:59 -0400

    add CI handling

--- scripts/docker/push.sh
+++ scripts/docker/push.sh
@@ -20,12 +20,13 @@ then
     echo "Pulling CI image"
     docker pull $FULL_REPOSITORY_NAME:ci
     echo "Re-tagging for prod"
-    docker tag $FULL_REPOSITORY_NAME:ci $FULL_REPOSITORY_NAME:$GIT_SHA:$ENV
+    docker tag $FULL_REPOSITORY_NAME:ci $FULL_REPOSITORY_NAME:$ENV
     echo "Pushing to prod image to ECR"
-    docker push $FULL_REPOSITORY_NAME:$GIT_SHA:$ENV
+    
     docker push $FULL_REPOSITORY_NAME:$ENV
 elif [[ $ENV == "ci" ]]
 then
     echo "Pushing to ECR in CI environment"
-    docker push $FULL_REPOSITORY_NAME:$GIT_SHA:$ENV
+    docker push $FULL_REPOSITORY_NAME:$GIT_SHA
+    docker push $FULL_REPOSITORY_NAME:$ENV
 fi

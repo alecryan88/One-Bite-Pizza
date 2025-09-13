f768299eacb4a7938b8ead659fd6df7d8a17e348
Alec Ryan <alecryan729@gmail.com>
2025-09-13 08:23:47 -0400

    add CI handling

--- scripts/docker/push.sh
+++ scripts/docker/push.sh
@@ -8,12 +8,8 @@ ECR_REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 FULL_REPOSITORY_NAME=$ECR_REGISTRY/$REPOSITORY_NAME
 GIT_SHA=$GITHUB_SHA
 
-echo "GIT_TAG: $GIT_TAG"
+echo "GIT_SHA: $GIT_SHA"
 echo "MAIN_TAG: $MAIN_TAG"
-
-# Login to ECR
-echo "Logging in to ECR"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 
 # Push the image
 echo "Pushing the image"

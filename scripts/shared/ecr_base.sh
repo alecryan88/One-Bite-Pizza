6feef49128fa6732af1029be3bda50c50b97f769
Alec Ryan <alecryan729@gmail.com>
2025-09-30 08:24:25 -0400

    Update name for stack

--- scripts/shared/ecr_base.sh
+++ scripts/shared/ecr_base.sh
@@ -9,7 +9,7 @@ ECR_REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # This is the full repository name for the project.
 # It includes the environment name.
-FULL_REPOSITORY_NAME=$ECR_REGISTRY/$PROJECT_NAME-$ENV
+FULL_REPOSITORY_NAME=$ECR_REGISTRY/$PROJECT_NAME
 
 export ECR_REGISTRY=$ECR_REGISTRY
 export FULL_REPOSITORY_NAME=$FULL_REPOSITORY_NAME
